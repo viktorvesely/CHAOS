f7d63bf90b9c46f6520a8fe4cb427ff31c4a17fd
Tovaris <30593536+viktorvesely@users.noreply.github.com>
2022-04-19 16:57:20 +0200

    awdda

--- heart/run_recorder.sh
+++ heart/run_recorder.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=heart_job
-#SBATCH --mem=4GB
+#SBATCH --mem=8GB
 #SBATCH --partition=regular
 module add Biopython/1.78-foss-2020a-Python-3.8.2
 python3 recorder.py --name traj_n --cores 1
